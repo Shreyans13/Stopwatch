f56dca9b30d68e6fcb54c4bc5d183a445d442006
Vivek <viveksonybcd@gmail.com>
2020-10-19 14:31:59 +0530

    Changed color theme and deleted redundant code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var startTimerButton = document.querySelector('.startTimer');
-var pauseTimerButton = document.querySelector('.pauseTimer');
-var timerDisplay = document.querySelector('.timer');
+var startTimerButton = document.querySelector(".startTimer");
+var pauseTimerButton = document.querySelector(".pauseTimer");
+var timerDisplay = document.querySelector(".timer");
 var startTime;
 var updatedTime;
 var difference;
@@ -15,14 +15,10 @@ function startTimer() {
     tInterval = setInterval(getShowTime, 1);
     paused = 0;
     running = 1;
-    timerDisplay.style.background = "#1b1b2f";
-    timerDisplay.style.cursor = "auto";
-    timerDisplay.style.color = "#ff5200";
-    timerDisplay.style.fontSize= "10rem";
-    startTimerButton.classList.add('lighter');
-    pauseTimerButton.classList.remove('lighter');
-    startTimerButton.style.cursor = "auto";
-    pauseTimerButton.style.cursor = "pointer";
+    timerDisplay.style.color = "#28df99";
+    timerDisplay.style.fontSize = "10rem";
+    startTimerButton.classList.add("lighter");
+    pauseTimerButton.classList.remove("lighter");
   }
 }
 
@@ -33,13 +29,9 @@ function pauseTimer() {
     savedTime = difference;
     paused = 1;
     running = 0;
-    timerDisplay.style.background = "#1b1b2f";
-    timerDisplay.style.color = "grey";
     timerDisplay.style.cursor = "pointer";
-    startTimerButton.classList.remove('lighter');
-    pauseTimerButton.classList.add('lighter');
-    startTimerButton.style.cursor = "pointer";
-    pauseTimerButton.style.cursor = "auto";
+    startTimerButton.classList.remove("lighter");
+    pauseTimerButton.classList.add("lighter");
   } else {
     startTimer();
   }
@@ -51,31 +43,35 @@ function resetTimer() {
   difference = 0;
   paused = 0;
   running = 0;
-  timerDisplay.innerHTML = 'Start Timer!';
-  timerDisplay.style.background = "#1b1b2f";
+  timerDisplay.innerHTML = "Start Timer!";
   timerDisplay.style.color = "grey";
   timerDisplay.style.cursor = "pointer";
-  startTimerButton.classList.remove('lighter');
-  pauseTimerButton.classList.remove('lighter');
-  startTimerButton.style.cursor = "pointer";
-  pauseTimerButton.style.cursor = "auto";
+  startTimerButton.classList.remove("lighter");
+  pauseTimerButton.classList.remove("lighter");
 }
 
 function getShowTime() {
   updatedTime = new Date().getTime();
   if (savedTime) {
-    difference = (updatedTime - startTime) + savedTime;
+    difference = updatedTime - startTime + savedTime;
   } else {
     difference = updatedTime - startTime;
   }
   // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((difference % (1000 * 60)) / 1000);
   var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
-  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds+ ' ' ;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  milliseconds =
+    milliseconds < 100
+      ? milliseconds < 10
+        ? "00" + milliseconds
+        : "0" + milliseconds
+      : milliseconds;
+  timerDisplay.innerHTML = hours + ":" + minutes + ":" + seconds + " ";
 }
